5f7a0a7c8afd84d96a352a76750d98541d849926
peej-s <peejmontero@gmail.com>
2020-04-25 01:31:48 -0400

    update auth and search

--- src/application.py
+++ src/application.py
@@ -2,32 +2,55 @@ from os import environ
 from flask import Flask, request, render_template
 import requests
 import json
+from datetime import datetime, timedelta
 
 app = Flask(__name__)
-ACCESS_TOKEN = environ.get('SPOTIFY_API_KEY')
+CLIENT_CREDENTIALS = environ.get('SPOTIFY_CLIENT_CREDENTIALS')
 SPOTIFY_API_URL = "https://api.spotify.com/v1"
 SEARCH_LIMIT = 20
 API_PATH = "/api/v1"
+API_ACCESS_TOKEN = ""
+TOKEN_EXPIRY = datetime.now()
 
 ## API
+
+def verify_token():
+    global API_ACCESS_TOKEN
+    global TOKEN_EXPIRY
+    global CLIENT_CREDENTIALS
+    if (TOKEN_EXPIRY is None) or (datetime.now() > TOKEN_EXPIRY):
+        current_time = datetime.now()
+        if (CLIENT_CREDENTIALS):
+            token_response = requests.post(
+                "https://accounts.spotify.com/api/token",
+                data={"grant_type": "client_credentials"},
+                headers={"Authorization": "Basic " + CLIENT_CREDENTIALS},
+            ).json()
+            API_ACCESS_TOKEN = token_response["access_token"]
+            TOKEN_EXPIRY = current_time + timedelta(seconds=int(token_response["expires_in"]))
+    return API_ACCESS_TOKEN
 
 # Keep this for debugging
 @app.route(API_PATH + '/key')
 def get_api_key():
-    if ACCESS_TOKEN:
-        return ACCESS_TOKEN
+    token = verify_token()
+    if token != "":
+        return token
     else:
-        return "No access token found"
+        # This should be the only time a user sees this message. If there is missing credentials
+        # and we are unable to hit the API, that's a server-side issue
+        return "No access token found. Did you set the SPOTIFY_CLIENT_CREDENTIALS variable?"
 
 
 @app.route(API_PATH + '/search')
 def search_tracks():
     q = request.args.get('q')
     if (q != ""):
+        token = verify_token()
         r = requests.get(
             SPOTIFY_API_URL + "/search",
             params={"q": q, "type": "track", "limit": SEARCH_LIMIT},
-            headers={"Authorization": "Bearer " + ACCESS_TOKEN},
+            headers={"Authorization": "Bearer " + token},
         ).json()
 
         returned_tracks = r["tracks"]["items"]
@@ -50,15 +73,17 @@ def search_tracks():
 # Keep this for debugging
 @app.route(API_PATH + '/analyze/<track_id>')
 def analyze_track(track_id):
+    token = verify_token()
     r = requests.get(
         SPOTIFY_API_URL + "/audio-features/" + track_id,
-        headers={"Authorization": "Bearer " + ACCESS_TOKEN},
+        headers={"Authorization": "Bearer " + token},
     ).json()
     return r
 
 
 @app.route(API_PATH + '/recommend/<track_id>')
 def recommend_tracks(track_id):
+    token = verify_token()
     track_data = analyze_track(track_id)
     request_params = {
         "seed_tracks": track_id,
@@ -69,9 +94,14 @@ def recommend_tracks(track_id):
     r = requests.get(
         SPOTIFY_API_URL + "/recommendations",
         params=request_params,
-        headers={"Authorization": "Bearer " + ACCESS_TOKEN}
+        headers={"Authorization": "Bearer " + token}
     ).json()["tracks"]
-    return r
+
+    track_name = requests.get(
+        SPOTIFY_API_URL + "/tracks/" + track_id,
+        headers={"Authorization": "Bearer " + token}
+    ).json()["name"]
+    return r, track_name
 
 ## Server-Side Rendering
 
@@ -81,13 +111,14 @@ def render_homepage():
 
 @app.route('/search')
 def render_search_tracks():
+    query = request.args.get('q')
     tracks = search_tracks()["tracks"]
-    return render_template("search.html", tracks=tracks, query=request.args.get('q'))
+    return render_template("search.html", tracks=tracks, query=query)
 
 @app.route('/recommend/<track_id>')
 def render_recommend_tracks(track_id):
-    recommended_tracks = recommend_tracks(track_id)
-    return render_template("recommend.html", tracks=recommended_tracks)
+    recommended_tracks, track_name = recommend_tracks(track_id)
+    return render_template("recommend.html", tracks=recommended_tracks, track_name=track_name)
 
 
 def main():
